2e615aeff18b0e9e438567dc71c31eee3292949d
Vasyl <vlazoryk@chnu.edu.ua>
2024-08-15 14:21:41 +0300

    new 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("Java Lab #7 Template");
+        System.out.printf("Java Lab #7 Template new");
         // write your code here
     }
 
